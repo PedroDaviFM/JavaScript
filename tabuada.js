43db7bf24bc7ed48c94214a5d8bac0a0a4d88663
PedroDaviFM <112030104+PedroDaviFM@users.noreply.github.com>
2025-08-05 15:32:52 -0300

    Tabuada
    Receba/armazena um número N e exiba a tabuada desse número, de 1 até 10.
    
    O desafio deve ser feito utilizando estruturas de repetição.

---
+++ tabuada.js
@@ -0,0 +1,18 @@
+function tabuada(num){
+    console.log(`\nA tabuada de ${num} é:\n`)
+    for(i = 1; i <= 10; i++){
+        let mult = i * num;
+        console.log(`${i} X ${num} = ${mult}`)
+    }
+}
+
+tabuada(1);
+tabuada(2);
+tabuada(3);
+tabuada(4);
+tabuada(5);
+tabuada(6);
+tabuada(7);
+tabuada(8);
+tabuada(9);
+tabuada(10);
